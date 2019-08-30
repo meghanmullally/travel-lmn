de6bec149a7a3bbb500bfb6a012ab9c29d4e28c0
Lawrence Fiesta <lfiesta86@gmail.com>
2019-08-29 17:51:15 -0700

    making sure i pulled everything

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <Home msg="Welcome to Your Vue.js App"/>
+    <Home/>
   </div>
 </template>
 
 <script>
-import Home from './views/Home/Home'
-<<<<<<< HEAD
-console.log(process.env, "another secret");
-=======
->>>>>>> b8edb5b9a6697b302e98a8c53465cb9ba5e518e1
+import Home from './views/Home/Home';
+
 export default {
   name: 'app',
   components: {
