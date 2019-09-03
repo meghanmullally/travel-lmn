776e49a15b9c62189b74dad75887c54291d48fed
Meghan Mullally <ms.mullally27@gmail.com>
2019-09-03 16:46:15 -0700

    Added section and p tag for the current time and question

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="home">
     <h1>This is the home page</h1>
+    <section class="home-section">
+      <!-- area for the clock  -->
+      <p class="time" v-text="currentTime"></p>
+    </section>
+
+<!-- where would you like to go? -->
+<p>{{question}}</p>
+
   </div>
 </template>
 <script>
@@ -17,6 +25,18 @@ import moment from 'moment';
         currentTime: null,
         childData: ""
       }
+    },
+    methods: {
+      updateCurrentTime() {
+        this.currentTime = moment().format('LTS');
+      },
+      updateMessage(variable) {
+        this.childData = variable;
+      }
+    },
+    created() {
+      this.currentTime = moment().format('LTS');
+      setInterval(() => this.updateCurrentTime(), 1 * 1000);
     }
   }
 
