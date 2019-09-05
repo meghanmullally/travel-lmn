7f56b2a1fb810338bf9f95d3114a1f766ea079cd
Meghan Mullally <ms.mullally27@gmail.com>
2019-09-04 17:32:43 -0700

    basic template code with export default code and import style.css

--- src/views/Chat/Chat.vue
+++ src/views/Chat/Chat.vue
@@ -3,3 +3,16 @@
     <h1>This is the chat page</h1>
   </div>
 </template>
+<script>
+export default {
+  name: 'Chat',
+  date(){
+    return {
+      msg: "Chat page"
+  }
+  }
+}
+</script>
+<style>
+@import './assets/style/style.css';
+</style>
