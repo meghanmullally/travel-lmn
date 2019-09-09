58f61f8ddddfd3ca8720b28805b2f93872f6b09b
Meghan Mullally <ms.mullally27@gmail.com>
2019-09-09 14:06:23 -0700

    added import moment to main.js, npm i vuex, added store info

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from "./router";
+import moment from 'moment';
+import Vuex from 'vuex';
 // import Vuetify from "./plugins/vuetify";
 
 
@@ -12,11 +14,43 @@ Vue.use(MdDatepicker);
 Vue.use(MdButton);
 Vue.use(MdField);
 
+Vue.use(Vuex)
+
+Vue.prototype.moment = moment
 
 Vue.config.productionTip = false
 
+export const store = new Vuex.Store({
+  state: {
+    city: '',
+    flightInfo: {
+      originCode: '',
+      destinationCode: '',
+      dateCode: ''
+    }
+  },
+  mutations: {
+    changeCity (state, payload) {
+      state.city = payload.newCity
+    },
+    changeFlightInfo(state, payload){
+      state.flightInfo = payload.newFlightInfo  
+    }
+    
+  },
+  getters: 
+  {
+    city: state => state.city,
+    flightInfo: state => state.flightInfo,
+  }
+})
+
+
+
 new Vue({
   router,
+  moment,
+  store,
   // Vuetify,
   render: h => h(App),
 }).$mount('#app')
