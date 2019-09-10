f5006a0d9094291ff7bd8d899bed39f027ee1cee
Meghan Mullally <ms.mullally27@gmail.com>
2019-09-10 15:29:08 -0700

    table able to sort by either topic

--- src/components/PriceContainer/Price.vue
+++ src/components/PriceContainer/Price.vue
@@ -1,126 +1,101 @@
 <template>
-  <div class="price">
-    <md-table v-model="searched" md-sort="airline" md-sort-order="asc" md-card md-fixed-header>
+  <div>
+    <md-table v-model="flights" md-card @md-selected="onSelect" md-sort="airline" md-sort-order="asc">
       <md-table-toolbar>
-        <div class="md-toolbar-section-start">
-          <h1 class="md-title">Flight Options</h1>
-        </div>
-
-        <md-field md-clearable class="md-toolbar-section-end">
-          <md-input placeholder="Search by Airline..." v-model="search" @input="searchOnTable" />
-        </md-field>
+        <h1 class="md-title">Flight Options</h1>
       </md-table-toolbar>
 
-      <md-table-empty-state
-        md-label="No users found"
-        :md-description="`No user found for this '${search}' query. Try a different search term or create a new user.`">
-        <md-button class="md-primary md-raised" @click="newUser">Create New User</md-button>
-      </md-table-empty-state>
-
-      <md-table-row slot="md-table-row" slot-scope="{ item }">
+      <md-table-row slot="md-table-row" slot-scope="{ item }" :class="getClass(item)" md-selectable="single">
         <md-table-cell md-label="ID" md-sort-by="id" md-numeric>{{ item.id }}</md-table-cell>
-        <md-table-cell md-label="Airline" md-sort-by="Airline">{{ item.airline }}</md-table-cell>
-        <md-table-cell md-label="Flight" md-sort-by="Flight">{{ item.flight }}</md-table-cell>
-        <md-table-cell md-label="Departure" md-sort-by="Departure">{{ item.departure }}</md-table-cell>
-        <md-table-cell md-label="Arrival" md-sort-by="Arrival">{{ item.arrival }}</md-table-cell>
-        <md-table-cell md-label="Price" md-sort-by="Price">{{ item.price }}</md-table-cell>
+        <md-table-cell md-label="Airline" md-sort-by="airline">{{ item.airline }}</md-table-cell>
+        <md-table-cell md-label="Flight" md-sort-by="flight">{{ item.flight }}</md-table-cell>
+        <md-table-cell md-label="Departure" md-sort-by="departure">{{ item.departure }}</md-table-cell>
+        <md-table-cell md-label="Arrival" md-sort-by="arrival">{{ item.arrival }}</md-table-cell>
+        <md-table-cell md-label="Price" md-sort-by="price">{{ item.price }}</md-table-cell>
       </md-table-row>
     </md-table>
+
+    <p>Selected:</p>
+    {{ selected }}
   </div>
 </template>
 
 <script>
-  const toLower = text => {
-    return text.toString().toLowerCase()
-  }
-
-  const searchByAirline = (items, term) => {
-    if (term) {
-      return items.filter(item => toLower(item.airline).includes(toLower(term)))
-    }
-
-    return items
-  }
-
   export default {
-    name: 'PriceSearch',
+    name: 'TableSingle',
     data: () => ({
-      search: null,
-      searched: [],
-      users: [
+      selected: {},
+      flights: [
         {
           id: 1,
-          Airline: "Alaska Arlines",
-          Flight: "38",
-          Time: "13:09",
-          Departure: "09/10/2019",
-          Arrival: "09/13/2019",
-          Price: "$480"
+          airline: "Alaska Arlines",
+          flight: "38",
+          time: "13:09",
+          departure: "09/10/2019",
+          arrival: "09/13/2019",
+          price: "$480"
         },
         {
           id: 2,
-          Airline: "Delta",
-          Flight: "456",
-          Time: "9:23",
-          Departure: "10/14/2019",
-          Arrival: "10/20/2019",
-          Price: "$543"
+          airline: "Delta",
+          flight: "456",
+          time: "9:23",
+          departure: "10/14/2019",
+          arrival: "10/20/2019",
+          price: "$543"
         },
         {
           id: 3,
-          Airline: "Spirit",
-          Flight: "10",
-          Time: "16:58",
-          Departure: "09/20/2019",
-          Arrival: "09/30/2019",
-          Price: "$120"
+          airline: "Spirit",
+          flight: "10",
+          time: "16:58",
+          departure: "09/20/2019",
+          arrival: "09/30/2019",
+          price: "$120"
         },
         {
           id: 4,
-          Airline: "Southwest Airlines",
-          Flight: "7854",
-          Time: "12:29",
-          Departure: "11/10/2019",
-          Arrival: "12/13/2019",
-          Price: "$340"
+          airline: "Southwest Airlines",
+          flight: "7854",
+          time: "12:29",
+          departure: "11/10/2019",
+          arrival: "12/13/2019",
+          price: "$340"
         },
         {
           id: 5,
-          Airline: "United Airlines",
-          Flight: "3856",
-          Time: "16:09",
-          Departure: "12/10/2019",
-          Arrival: "12/13/2019",
-          Price: "$500"
+          airline: "United Airlines",
+          flight: "3856",
+          time: "16:09",
+          departure: "12/10/2019",
+          arrival: "12/13/2019",
+          price: "$500"
         },
         {
           id: 6,
-          Airline: "American Arlines",
-          Flight: "118",
-          Time: "06:09",
-          Departure: "01/10/2020",
-          Arrival: "01/13/2020",
-          Price: "$786"
+          airline: "American Arlines",
+          flight: "118",
+          time: "06:09",
+          departure: "01/10/2020",
+          arrival: "01/13/2020",
+          price: "$786"
         }
       ]
     }),
     methods: {
-      newUser () {
-        window.alert('Noop')
-      },
-      searchOnTable () {
-        this.searched = searchByAirline(this.users, this.search)
+      getClass: ({ id }) => ({
+        'md-primary': id === 2,
+        'md-accent': id === 3
+      }),
+      onSelect (item) {
+        this.selected = item
       }
-    },
-    created () {
-      this.searched = this.users
     }
   }
 </script>
 
 <style>
-  .md-field {
-    width: 570px;
-    height: auto;
+  .md-table + .md-table {
+    margin-top: 16px
   }
 </style>
