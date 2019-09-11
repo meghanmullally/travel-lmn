bcbdfaba1db540bf0169171a177b573191bd9252
Meghan Mullally <ms.mullally27@gmail.com>
2019-09-10 17:33:40 -0700

    created a Favorties, Itinerary, User and Past trips comp with files and the basic templates

---
+++ src/components/Favorites/Favorites.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="favorite">
+    <article class="container">
+      <!-- Favorite destinations!  -->
+      <section class="favorite">
+      <h3>fav destinations?</h3>
+      </section>
+    </article>
+    
+    
+    
+    
+  </div>
+</template>
+<script>
+export default {
+  name: 'favorite',
+  date(){
+    return {
+      msg: "favorite"
+  }
+  }
+}
+</script>
+<style>
+/* @import './assets/styles/style.css'; */
+</style>
---
+++ src/components/UserInfo/User.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="user">
+    <article class="container">
+    <!-- User Profile Info -->
+      <section class="user">
+      <h3>User Information</h3>
+      <p>Bio</p>
+      <p>maybe an image?</p>
+      <p>Name:</p>
+      <p>Age:</p>
+      <p>Email:</p>
+      </section>
+    </article>
+    
+    
+    
+    
+  </div>
+</template>
+<script>
+export default {
+  name: 'user',
+  date(){
+    return {
+      msg: "user"
+  }
+  }
+}
+</script>
+<style>
+/* @import './assets/styles/style.css'; */
+</style>
