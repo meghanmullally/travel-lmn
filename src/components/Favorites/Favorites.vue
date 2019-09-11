29a506b919a78c9304cbf0d5dbd4ae2b4886994f
Meghan Mullally <ms.mullally27@gmail.com>
2019-09-10 22:14:33 -0700

    added some images to favorites juses just for some hard coding images right now

--- src/components/Favorites/Favorites.vue
+++ src/components/Favorites/Favorites.vue
@@ -3,7 +3,48 @@
       <!-- Favorite destinations!  -->
       <section class="favorite">
       <h3>fav destinations?</h3>
-      <p>HELLO</p>
+      <md-card>
+      <md-card-media-actions>
+        <md-card-media>
+          <img src="/assets/examples/villains.png" alt="Cover">
+        </md-card-media>
+
+        <md-card-actions>
+          <md-button class="md-icon-button">
+            <md-icon>favorite</md-icon>
+          </md-button>
+
+          <md-button class="md-icon-button">
+            <md-icon>bookmark</md-icon>
+          </md-button>
+
+          <md-button class="md-icon-button">
+            <md-icon>share</md-icon>
+          </md-button>
+        </md-card-actions>
+      </md-card-media-actions>
+    </md-card>
+    <md-card>
+      <md-card-media-actions>
+        <md-card-media>
+          <img src="/assets/examples/villains.png" alt="Cover">
+        </md-card-media>
+
+        <md-card-actions>
+          <md-button class="md-icon-button">
+            <md-icon>favorite</md-icon>
+          </md-button>
+
+          <md-button class="md-icon-button">
+            <md-icon>bookmark</md-icon>
+          </md-button>
+
+          <md-button class="md-icon-button">
+            <md-icon>share</md-icon>
+          </md-button>
+        </md-card-actions>
+      </md-card-media-actions>
+    </md-card>
       </section>
   </div>
 </template>
@@ -18,5 +59,5 @@ export default {
 }
 </script>
 <style>
-/* @import './assets/styles/style.css'; */
+@import './assets/styles/style.css';
 </style>
