bb2828239d3f712571f95b86e518a41bc26b56e9
Meghan Mullally <ms.mullally27@gmail.com>
2019-09-11 00:49:43 -0700

    created a fav activites comp with bsic structure, assets folder with imags and css

---
+++ src/components/FavoriteActivities/FavoriteActivities.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="FavoriteActivites">
+    
+  </div>
+</template>
+<script>
+export default {
+  name: 'Favorites',
+  date(){
+    return {
+      msg: "favorite"
+  }
+  }
+}
+</script>
+<style>
+@import './assets/styles/style.css';
+</style>
