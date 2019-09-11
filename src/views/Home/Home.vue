f9ff32e6fcef8464c7ff07144a47553590a3830f
Meghan Mullally <ms.mullally27@gmail.com>
2019-09-11 12:58:12 -0700

    took out toolbar code

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,44 +1,6 @@
 <template>
   <div class="Home">
     <!-- <h1>This is the home page</h1> -->
-    <!-- <md-toolbar class="md" id="navigation"> -->
-        <!-- <md-button class="md-icon-button" @click="toggleMenu" v-if="!menuVisible"> -->
-          <!-- <md-icon>menu</md-icon> -->
-        <!-- </md-button> -->
-        <!-- <span class="md-title">LMN</span> -->
-      <!-- </md-toolbar> -->
-
-      <md-drawer :md-active.sync="menuVisible" md-persistent="full" id="drawer">
-        <md-toolbar class="md-transparent" md-elevation="0">
-          <span>Navigation</span>
-          <div class="md-toolbar-section-end">
-            <md-button class="md-icon-button md-dense" @click="toggleMenu">
-              <md-icon>keyboard_arrow_left</md-icon>
-            </md-button>
-          </div>
-        </md-toolbar>
-        <md-list id="nav-list">
-          <md-list-item>
-            <md-icon>card_travel</md-icon>
-            <span class="md-list-item-text">Destination</span>
-          </md-list-item>
-
-          <md-list-item>
-            <md-icon>explore</md-icon>
-            <span class="md-list-item-text">Activities</span>
-          </md-list-item>
-
-          <md-list-item>
-            <md-icon>supervisor_account</md-icon>
-            <span class="md-list-item-text">Profile</span>
-          </md-list-item>
-
-          <md-list-item>
-            <md-icon>chat</md-icon>
-            <span class="md-list-item-text">Chat</span>
-          </md-list-item>
-        </md-list>
-      </md-drawer>
     <section class="home-section">
       <!-- area for the clock  -->
       <p class="time" v-text="currentTime"></p>
