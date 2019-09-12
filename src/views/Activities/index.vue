e0bff67e982c9f4d44abfad49e2eed7c50558227
Lawrence Fiesta <lfiesta86@gmail.com>
2019-09-12 16:47:56 -0700

    got the youtube api to work and return the data that i need. did not style it though

--- src/views/Activities/index.vue
+++ src/views/Activities/index.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="Activities">
     <!-- <h1>This is the Youtube and Map page?</h1> -->
-    <!-- <YoutubeContainer/> -->
+    <YoutubeContainer/>
     <GoogleMap/>
   </div>
 </template>
 <script>
 // import YoutubeContainer from '../../components/YoutubeContainer/Youtube';
 import GoogleMap from '../../components/GoogleMap/Map';
+import YoutubeContainer from '../../components/YoutubeContainer';
 
 export default {
   name: 'Activities',
   components: {
-    // YoutubeContainer,
+    YoutubeContainer,
     GoogleMap
   },
   date(){
